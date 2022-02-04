ccd72c7da7efdd96e6084de9a0cd4124286e3905
BaoLanNguyen <89245290+BaoLanNguyen@users.noreply.github.com>
2022-02-04 14:15:17 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            Console.WriteLine("Hello World!");
+            watch.Stop();
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+
+            
+        }
+    }
+}
+
