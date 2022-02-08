9407aa2236e8266570ce33ce114b8f5c043f2670
BaoLanNguyen <bang03001@utb.vaxjo.se>
2022-02-08 14:21:12 +0100

    su

--- Program.cs
+++ Program.cs
@@ -3,15 +3,41 @@ namespace sort
 {
     class Program
     {
+        static void swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b= temp;
+        }
+
+        static void bubblesort(ref int[] a){
+        for (int i = 0; i < a.Length - 1; i++)
+            {
+                for (int j = 0; j < a.Length - i - 1; j++)
+                {
+                    if (a[j] > a[j+1])
+                    {
+                        swap(ref a[j], ref a[j+1]);
+                    }
+                }
+            }
+}
+        static void selectionsort(ref int[] a, ref int n){
+
+        }
         static void Main(string[] args)
         {
-            var watch = new System.Diagnostics.Stopwatch();
-            watch.Start();
-            Console.WriteLine("Hello World!");
-            watch.Stop();
-            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
 
-            
+            int[] arr = {2000, 1000, 8000, 4000};
+            long startTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            bubblesort(ref arr);
+            long stopTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            long elapsedTime = stopTime - startTime;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine(elapsedTime);
         }
     }
 }
